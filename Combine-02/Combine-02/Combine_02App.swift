c6fca12cf9b5e55babd244844e3ec72e6879b7fc
Bran <cold929@naver.com>
2022-08-29 20:54:16 +0900

    chore: project init

---
+++ Combine-02/Combine-02/Combine_02App.swift
@@ -0,0 +1,17 @@
+//
+//  Combine_02App.swift
+//  Combine-02
+//
+//  Created by Bran on 2022/08/29.
+//
+
+import SwiftUI
+
+@main
+struct Combine_02App: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
